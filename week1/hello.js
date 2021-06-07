27b1a18dede4c7786450a8f1a86ad8846ab9e769
patto1878 <1878patto@gmail.com>
2021-06-07 12:44:21 +0100

    update1

---
+++ week1/hello.js
@@ -0,0 +1,434 @@
+// console.log("hello");
+
+// console.log("hello".length);
+
+// console.log("hello".toUpperCase());
+
+// console.log(Math.random())
+
+// console.log(Math.random()*10);
+
+// console.log(Math.floor(Math.random()*10));
+
+// console.log(Math.ceil(Math.random()*10));
+
+// let num = Math.random()*10;
+
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log("------");
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log("------");
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log(" | | ");
+
+
+// console.log("All Around the world".charAt(7).toUpperCase());
+
+
+// // console.log this is how to make notes. 
+
+// let favouriteDrink = "coffee";
+
+// let age = "35"
+
+// let name = "Bryan"
+
+// let favcolour = "Yellow"
+
+// let brekkie = "cereal"
+
+// let dinner = "burger"
+
+// let tea = "Spaghetti"
+
+// console.log( favouriteDrink)
+
+// console.log("my fav drink is "+favouriteDrink);
+
+// console.log(`my fav drink is ${favouriteDrink}`);
+
+// console.log(name)
+
+// console.log(age)
+
+// console.log(favcolour)
+
+// console.log (`for breakfast i had ${brekkie}, for dinner i had ${dinner}, for tea i had ${tea}`)
+
+// console.log(`My name is ${name}, i am ${age} and my favourite colour is ${favcolour}`);
+
+// brekkie = "toast"
+
+// dinner = "Soup"
+
+// tea = "Pizza"
+
+// console.log (`tomorrow for breakfast i will have ${brekkie}, then i will have ${dinner} for lunch and ${tea} for tea`);
+
+// const birthday = new Date();
+// const today = new Date(); 
+// birthday.setFullYear(2021, 6, 22);
+// const diff =Math.floor(((((birthday - today)/1000) /60) /60) /24);
+// console.log(`My birthday is in ${diff} days`);
+
+// let space1 = "X"
+// let space2 = "O"
+
+// console.log( `${space1}  |   |   `);
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log("------");
+// console.log(" | | ");
+// console.log(" |`${space2} | `");
+// console.log(" | | ");
+// console.log("------");
+// console.log(" | | ");
+// console.log(" | | ");
+// console.log(" | | ");
+
+
+// let music = "edm";
+
+// if (music == "rock") {
+// console.log("oh i love this song");
+// }
+// else if (music == "no music") {
+// console.log("i cant hear anything");
+// }
+// else {
+// console.log("i dont recognise this song");
+// }
+
+// if (1234 % 1234){
+//     console.log("true");
+// }
+// else console.log("false")
+
+
+// if (1 === "1") {
+//     console.log(true);
+//     }
+//     else {
+//     console.log(false);
+//     }
+
+//     let place = "Manc";
+//     let weather = "Cloudy";
+//     if (place == "Manc" && weather == "Sunny") {
+//     console.log("Check again");
+//     }
+//     else if (place == "Manc" && weather == "Rain") {
+//     console.log("Obvs");
+//     }
+//     else {
+//     console.log("What it isn't raining?");
+//     }
+
+
+//     let car = "Peugeot";
+// if(car == "Ford" || car == "GM"){
+// console.log("You've got an American car!");
+// }
+// else if(car == "Peugeot" || car == "Citroen"){
+// console.log("You've got a French boy!");
+// }
+// else if(car == "Honda" || car == "Toyota" || car == "Suzuki"){
+// console.log("Japanese cars are dead quiet!");
+// }
+// else if(car == "Mercedes"){
+// console.log("You are proper posh German!");
+// }
+// else if(car == "Volkswagen"){
+// console.log("German aren't that bad at all!");
+// }
+// else if(car == "Hyundai" || car == "Kia"){
+// console.log("South Korean cars are getting popular!");
+// }
+// else{
+// console.log("Your car is not in the top ten companies in the world!");}
+
+
+// const grade = 22;
+
+// switch (true){
+//     case grade >= 70:
+//         console.log("distinction");
+//         break;
+//     case grade >= 60:
+//         console.log ("merit");
+//         break;
+//     case grade >= 50:
+//         console.log ("pass");
+//         break;
+//     default:
+//         console.log("fail");
+// }
+
+// let age = 22;
+// let age 
+// let countryUSA = USA
+// let countryUK = UK 
+
+
+// if (age >= 18 && country = UK){
+//     console.log("heres a pint my british mate")
+// }
+//     else if (age > 17 && country UK){
+//     console.log("you are not old enough")
+//     else if (age <21 && country = USA)
+//     console.log(sorry not old enough)
+//     }
+
+
+// let age = 18;
+// let country = "UK";
+
+// if (age >= 18 && country == "UK"){
+// console.log("Here's a beer my British friend!");
+// } else if (age >= 21 && country == "USA") {
+// console.log("Here's a beer my American friend!");
+// } else {
+// console.log("Here's a J20...")
+// }
+
+
+// let topping = "chicken";
+
+// switch (topping) {
+// case "cheese":
+// case "onion":
+// case "quorn":
+// console.log("These are important ingredients for my pizza");
+// break;
+// case "chicken":
+// console.log(`I like having ${topping} on pizza.`);
+// break;
+// case "pineapple":
+// console.log(`${topping} should not be on a pizza.”`);
+// break;
+// default:
+// console.log("It is not food!");
+// }
+
+// let password = "efc1878";
+
+// if (password.length < 8) {
+// console.log ("Sorry Your password is too short.");
+// }
+// else {
+// console.log(`Correct, Your password is ${password}.`);
+// }
+
+
+// let num = 27
+
+// if (num % 3 == 0 || num % 5 == 0){
+//     console.log ("correct")
+// }else {
+//         console.log("maths doesnt work sorry");
+//     }
+
+
+// let num = 9;
+
+// if (num % 3 == 0 || num % 5 == 0){
+//     console.log ("fizzbuzz");
+// }else if (num % 3 == 0) {
+//         console.log("fizz");}
+//  else if (num % 5 == 0){
+//     console.log("buzz")}
+    
+
+// let time = 7;
+// let poh = "Liverpool";
+// let pow = "Manc";
+    
+// if time >= 7 {
+// console.log(poh);}
+
+// if (1 === "1") {
+//     console.log(true);
+// }
+// else {console.log(false);}
+
+// let age = 12;
+
+// if(age <18){
+// console.log("Thats a child ticket then, £8 please.");
+
+// }
+// else if(age >= 18 && age < 60){
+// console.log("Your ticket will be the standard £10.50");
+// }
+// else if(age >= 60){
+// console.log("one oap ticket, thats £7.50");
+// }
+// else {
+// console.log("Input not valid");
+// }
+
+
+// below is how to ake a FUNCTION (3 examples)
+
+// const pressGrindBeans = ()  => {
+//     console.log ("grinding for 20 seconds");
+
+// }
+//     pressGrindBeans ();
+
+// let bool = true;
+
+// if(bool){
+// console.log("True");
+// } else {
+// console.log("False");
+// }
+
+// let coffeeIsGrinding = false;
+// const pressGrindBeans = () => {
+// if(coffeeIsGrinding){
+// console.log("Coffee is grinding.");
+// coffeeIsGrinding = true;
+// }
+// else{
+// console.log("lets get grinding then");
+// coffeeIsGrinding = false;
+// }
+// }
+
+// pressGrindBeans();
+
+// let amount = 20
+// let accnum = 50449921;
+// const cashWithdrawel = (amount, accnum) => {
+//     console.log(`withdrawing ${amount} from account ${accnum}`);
+// }
+
+
+// cashWithdrawel (300, 50449921);
+// cashWithdrawel (30 ,50449921);
+// cashWithdrawel (200, 50447921);
+
+
+// const addUp = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// addUp (7,3);
+// console.log(addUp(2,5));
+
+// funtions ecamples continued
+
+// const multiplyByNineFifths = (celsius) => {
+//     return celsius * (9/5);
+//     };
+//     const getFahrenheit = (celsius) => {
+//     return multiplyByNineFifths(celsius) + 32;
+//     };
+//     console.log("The temperature is " + getFahrenheit(15) + "°F");// Output: The temperature is 59°F
+    
+// function coffeeOrder(size,type) {
+//     console.log(`Here you go, one ${size} ${type}.`);
+//     }
+//     coffeeOrder(`large`,`tea`);
+
+// 2 coffee function example, bottom one uses arrow syntax
+
+// const coffeOrder = (size, drink) => {
+//     console.log(`order is ${size} ${drink}`);
+//     }
+    
+//     coffeOrder("large", "coffee");
+
+// const factorial = (n) => {
+//     if ((n === 0) || (n === 1)) {
+//     return 1;
+//     } else {
+//     return (n * factorial(n-1));
+//     }
+//     }
+//     console.log(factorial(33));
+
+
+// pizza order that counts orders below
+
+
+// let orderCount = 0
+
+// const takeOrder = (topping, topping2) =>{
+//     console.log(`pizza with ${topping} and ${topping2}`);
+//     orderCount++;
+// }
+
+// takeOrder("pineapple","ham")
+// takeOrder("pineapple","ham")
+// console.log(orderCount)
+
+// cash machine example
+
+// let availableCash = 1000
+// let pin = 0000
+// const cashWithdrawal = (cash) => {
+// availableCash = availableCash - cash;
+// console.log(`You have withdrawn ${cash} from your account. Your remaining balance is ${availableCash}`);
+// }
+// const amountCredentials = (amount, pin) => {
+// if (pin !== 0000) {console.log(`Your PIN number was incorrect.`)}
+// else if (amount > availableCash) {`You have insufficient funds to make this withdrawal.`}
+// else {cashWithdrawal(amount)}
+// }
+// amountCredentials(200, 0000)
+
+
+// grids and arrays examples
+
+// let grid = [
+//     ['x', 'x', 'x'],
+//     ['4', '5', '6'],
+//     ['7', '8', '9']
+//     ];
+    
+//     console.log(grid[0][0]); // Output: 1
+//     console.log(grid[0][1]); // Output: 2
+//     console.log(grid[0][2]); // Output: 3
+//     console.log(grid[1][0]); // Output: 4
+//     console.log(grid[1][1]); // Output: 5
+//     console.log(grid[1][2]); // Output: 6
+//     console.log(grid[2][0]); // Output: 7
+//     console.log(grid[2][1]); // Output: 8
+//     console.log(grid[2][2]); // Output: 9
+
+//     if ((grid[0][0] == "x" && grid[0][1] == "x" && grid[0][2] == "x") || (grid[0][0] == "o" && grid[0][1] == "o" && grid[0][2] == "o")){
+//         console.log("WINNER WINNER!");
+//         }
+
+// grid above info. numbers 1-9 in grid oridginally, swap with xs to get winner winner, essentially a naughts and crosses board
+
+
+let pin = true;
+let op1 = "Withdraw";
+let op2 = "Check balance";
+let availBal = 500;
+let withdraw = 200;
+const getCash = () => {
+    if (pin === true) {
+        console.log("Pin correct. Select option: Withdraw or Check balance");
+        console.log(`your available balance is ${availBal} pounds`  );
+        console.log("How much would you like to withdraw?");
+        if (withdraw <= availBal) {
+            console.log(`You have selected to withdraw ${withdraw} pounds.`);
+        } else {
+            console.log("Not enough funds");
+        }
+    } else {
+        console.log("Incorrect Pin");
+    }
+}
+getCash ();
+
+
